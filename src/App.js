c0e4482361d1b3f5dc175201c814eee31f4c544e
michaels90up <michael1990spw@gmail.com>
2022-09-12 14:59:20 +0200

    Add instructions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Random-Color-Generator</h1>
+      <br />
+      <br />
+      <button>Generate</button>
+      <br />
+      <br />
+      <div>Generated Color:</div>
+      {/* change of the background color and show <background color hex code>*/}
+    </div>
+  );
 }
 
 export default App;
