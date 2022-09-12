cb3361a24bc18d992217df966675aa43f30e8195
michaels90up <michael1990spw@gmail.com>
2022-09-12 15:42:27 +0200

    Adding library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import './App.css';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('#40639c');
   return (
     <div>
       <h1>Random-Color-Generator</h1>
+      <div
+        style={{
+          width: 200,
+          height: 200,
+          margin: '0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+        }}
+      >
+        Generated Color: {color}
+      </div>
       <br />
       <br />
-      <button>Generate</button>
+      <br />
+      <button
+        onClick={() => {
+          setColor('');
+        }}
+      >
+        Generate
+      </button>
       <br />
       <br />
-      <div>Generated Color:</div>
-      {/* change of the background color and show <background color hex code>*/}
     </div>
   );
 }
